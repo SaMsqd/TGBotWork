17ae3e6d09adc0f2fb6db9d6481254f887f38ea8
SaMsqd <arh1pov.sam@yandex.ru>
2024-02-14 11:39:05 +0300

    прописал все функции из архитектуры

--- fullRemake/bot_logic.py
+++ fullRemake/bot_logic.py
@@ -3,18 +3,63 @@ from telebot.types import Message
 
 
 TOKEN = '2054290165:AAGo7Dqybp5fkqORKccJZdmZTXNcohdpAKw'
-
-if __name__ == '__main__':
-    bot = telebot.TeleBot(token=TOKEN)
+bot = telebot.TeleBot(token=TOKEN)
 
 
-@bot.message_handler(content_types=['text'])
+def check_user_id(user_id):
+    """
+    Проверка на наличие БД пользователя. Создание бд, если её нет
+    :param user_id:
+    :return:
+    """
+    flag = True
+    if not flag:   # TODO: Если id нет, создаём бд
+        return True
+
+
+@bot.message_handler(commands=['start'])
 def start(message: Message):
+    check_user_id(message.chat.id)
     bot.send_message(chat_id=message.chat.id, text=message.text + 's')
 
 
-if __name__ == '__main__':
-    bot.polling(non_stop=True, restart_on_change=True)
+@bot.message_handler(commands=['help'])
+def help(message: Message):
+    pass
+
+
+@bot.message_handler(commands=['number'])
+def number(message: Message):
+    pass
+
+
+@bot.message_handler(commands=['clear'])
+def clear(message: Message):
+    pass
+
+
+@bot.message_handler(commands=['table_best'])
+def table_best(message: Message):
+    pass
+
+
+@bot.message_handler(commands=['table_opt'])
+def table_opt(message: Message):
+    pass
+
+
+@bot.message_handler(commands=['table'])
+def table(message: Message):
+    pass
+
+
+@bot.message_handler(commands=['table_retail'])
+def table_retail(message: Message):
+    pass
+
+
+print('Бот запущен')
+bot.polling(non_stop=True)
 
 
 
---
+++ fullRemake/keyboard.py
@@ -0,0 +1,8 @@
+class Keyboard:
+    def generate_keyboard(self, keyboard_status):
+        """
+        Генерирует клавиатуру в зависимости от статуса клавиатуры
+        :param str keyboard_status:
+        :return:
+        """
+        pass
