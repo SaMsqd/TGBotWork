643450ea3ae5b3f5da0eeab6ca4d50ea9671a62a
Arhipov Sam <arh1pov.sam@yandex.ru>
2024-09-11 16:23:58 +0300

    Init new bot

--- Dockerfile
+++
@@ -1,8 +0,0 @@
-from python:3.8
-
-COPY requirements.txt .
-RUN pip install --no-cache-dir -r requirements.txt
-
-COPY . .
-
-CMD ["python", "./fullRemake/bot_logic.py"]
--- keyboard.py
+++ keyboard.py
@@ -1,35 +1,13 @@
 from telebot.types import ReplyKeyboardMarkup, KeyboardButton
-from project_databases import databases
 
 
-class Keyboard:
-    def __init__(self):
-        self.active_buttons = ['/help', '/clear', '/number', '/table', '/table_best',
-                               '/table_opt', '/table_retail', '/back']
-        self.start_buttons = ['/help', '/phones', '/watches', '/airpods', '/macbooks', '/ipads']
-        self.pos = 'back'
-        self.active_database = None
+Keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
+text = ['/number', '/clear', '/table',
+        '/table_best', '/table_opt', '/table_retail',
+        '/table_retail_file', '/table_1000']
 
-    def change_pos(self, new_pos: str):
-        new_pos = new_pos.replace("/", "")
-        self.pos = new_pos if '/' + new_pos in self.start_buttons[1:] else 'back'
-        self.active_database = databases[new_pos] if '/' + new_pos in self.start_buttons[1:] else None
-        if self.active_database:
-            return f'База данных успешно выбрана: {new_pos}'
-        elif new_pos == 'back':
-            return 'Вы были перемещены на стартовую позицию'
-        else:
-            return 'Ввод был не правильный, вы были перемещены на стартовую позицию'
+buttons = []
+for t in text:
+    buttons.append(KeyboardButton(text=t))
 
-    def generate_keyboard(self) -> ReplyKeyboardMarkup:
-        keyboard = ReplyKeyboardMarkup(resize_keyboard=True, row_width=3)
-        row = list()
-        buttons = self.start_buttons if self.pos == 'back' else self.active_buttons
-        for i in range(len(buttons)):
-            row.append(KeyboardButton(text=buttons[i]))
-            if i % 3 == 0:
-                keyboard.row(*row)
-                row = []
-        if len(row) != 0:
-            keyboard.row(*row)
-        return keyboard
+Keyboard.add(*buttons)
